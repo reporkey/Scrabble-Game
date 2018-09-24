8ba0d23fa498d63441c22e9689f0925834776c83
reporkey <zhuzichunhogan@gmail.com>
2018-09-24 17:04:40 +1000

    s

--- Server/src/Server/World.java
+++ Server/src/Server/World.java
@@ -1,19 +1,26 @@
 package Server;
 
+import org.json.JSONObject;
 import javax.net.ServerSocketFactory;
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
 import java.util.ArrayList;
 
 public class World {
 
     private static final int port = 8080;
-    private ArrayList<Player> players = new ArrayList<Player>();
+    public volatile ArrayList<Player> players = new ArrayList<Player>();
+    private char[][] map = new char[20][20];
+    public volatile String word = null;
+    private int pass = 0;
 
     public World() {
 
         ServerSocketFactory factory = ServerSocketFactory.getDefault();
+
+        // Game begin
+        Thread game = new Thread(new Game());
+        game.start();
 
         try(ServerSocket server = factory.createServerSocket(port)){
             System.out.println("Waiting for client connection..");
@@ -23,32 +30,184 @@ public class World {
                 Socket client = server.accept();
                 System.out.println("Client: Applying for connection!");
 
-
-                // Start a new thread for a connection
-                Thread t = new Thread(() -> serveClient(client));
-                t.start();
+                // Start a new thread to read msg
+                Thread myListener = new Thread(() -> listener(client));
+                myListener.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void serveClient(Socket client){
-        try(Socket clientSocket = client){
+    private void listener(Socket client) {
 
+        try {
             BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream(), "UTF-8"));
+            String msgStr = null;
 
-            String msg = null;
-            while ((msg = in.readLine()) != null){
+            while ((msgStr = in.readLine()) != null) {
                 // if request connection
-                Player player = new Player(client);
+
+                JSONObject msg = new JSONObject(msgStr);
+
+                if (msg.getString("method").equals("establish")) {
+                    Player player = new Player(client);
+                    players.add(player);
+                }
+                if (msg.getString("method").equals("ready")) {
+                    for (Player player : players) {
+                        if (client.getInetAddress().equals(player.getIp()) && client.getPort() == player.getPort()) {
+                            player.setReady();
+                        }
+                    }
+                }
+                if (msg.getString("method").equals("word")) {
+                    // if players pass, get empty word
+                    if (msg.getString("value").equals("") || msg.getString("value").equals(null)) {
+                        pass++;
+                        if (pass >= players.size()) {
+                            exit();
+                        }
+                    } else {
+                        pass = 0;
+                        word = msg.getString("value");
+                    }
+                }
+                if (msg.getString("method").equals("vote")) {
+                    for (Player player : players) {
+                        player.setVote(msg.getInt("value"));
+                    }
+                }
+                if (msg.getString("method").equals("end")) {
+                    exit();
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public class Game implements Runnable {
+
+        public void run() {
+
+            // Check ready
+            while(true) {
+                int ready = 0;
+                for (Player player : players) {
+                    if (player.getReady() == 1) {
+                        ready++;
+                    }
+                }
+                if (ready >= players.size()) {
+                    break;
+                }
+            }
+
+            // game begin
+            while (true) {
+                // iterate player's turn
+                for (Player player : players) {
+
+                    // send whose turn
+                    JSONObject msg = new JSONObject();
+                    msg.put("method", "begin");
+                    msg.put("map", map);
+                    msg.put("player",player);
+                    boardcast(msg);
+
+                    // wait until receive a word
+                    while (true) {
+                        try {
+                            Thread.sleep(50);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        if (!word.equals(null)) {
+                            break;
+                        }
+                    }
+
+                    // the player does not pass
+                    if (!word.equals("")){
+                        msg = new JSONObject();
+                        msg.put("method", "vote");
+                        msg.put("value", word);
+                        msg.put("map", map);
+                        msg.put("player",player);
+                        boardcast(msg);
+                    }
+
+                    // wait until receive all vote
+                    int vote;
+                    while (true) {
+                        try {
+                            Thread.sleep(50);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        vote = 0;
+                        for (Player temp : players){
+                            if (temp.getVote() == 1) {
+                                vote++;
+                            }else if (temp.getVote() == -1){
+                                vote = -1;
+                                break;
+                            }
+                        }
+                        if (vote == -1 && vote == players.size()) {
+                            break;
+                        }
+                    }
+
+                    // if all player vote yes
+                    if (vote == players.size()){
+                        player.setScore(player.getScore() + word.length());
+                    }
+
+                    // if map full
+                    for (char[] row : map){
+                        for(char point : row){
+                            if (point == '\u0000'){
+                                exit();
+                            }
+                        }
+                    }
+
+                }
             }
 
 
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+    }
+
+// methods
+
+    public void boardcast(JSONObject msg){
+
+        for (Player player : players){
+            try {
+                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(player.getSocket().getOutputStream(), "UTF" +
+                        "-8"));
+                out.write(msg.toString());
+                out.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void exit(){
+
+        JSONObject msg = new JSONObject();
+        msg.put("method", "end");
+        msg.put("value", players);
+        msg.put("map", map);
+
+        boardcast(msg);
+
+        System.out.println("Game Over. Server Closed.");
+        System.exit(0);
     }
 
 }
